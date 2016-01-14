01f8d5a33bb97a2c72679675ef98c4318ac70779
Filip Pytloun <filip@pytloun.cz>
2016-01-14 23:35:02 +0100

    Template tag faq_last

--- mezzanine_faq/templatetags/faq_tags.py
+++ mezzanine_faq/templatetags/faq_tags.py
@@ -15,3 +15,13 @@ def faq_list(**kwargs):
         'faq_questions': page.faqquestion_set.all(),
         'MEDIA_URL': settings.MEDIA_URL,
     }
+
+
+@register.inclusion_tag('includes/faqlist.html')
+def faq_last(**kwargs):
+    page = FaqPage.objects.get(**kwargs)
+    return {
+        'page': page,
+        'faq_questions': page.faqquestion_set.all().order_by('-id')[:1],
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
